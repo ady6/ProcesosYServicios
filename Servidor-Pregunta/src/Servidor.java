6970f7c33a5362402f4281e1ed9af05dd7241483
ady6 <82877241+ady6@users.noreply.github.com>
2022-01-01 19:50:30 +0100

    Servidor_preguntas_RMI

---
+++ Servidor-Pregunta/src/Servidor.java
@@ -0,0 +1,30 @@
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Servidor {
+	public static void main(String[] args) {
+		String host;
+		int puerto = 5051;
+		try {
+			host = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			System.out.println("No se ha podido obtener la dirección IP");
+			System.out.println(e.getMessage());
+			return;
+		}
+		try {
+			Registry registro = LocateRegistry.createRegistry(puerto);
+			TestRMI test = new TestRMI();
+			registro.rebind("miTest", test); //Añade el objeto test creado al registro de objetos remotos
+			System.out.println("Servicio registrado en host " + host + " y puerto " + puerto);
+		} catch (RemoteException e) {
+			System.out.println("No se ha podido registrar el servicio");
+			System.out.println(e.getMessage());
+		}
+	}
+}
+
